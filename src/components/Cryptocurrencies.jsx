effb4e556e40f119ac9e23b18f390fe6461e72f9
EvertonAdame <87421330+EvertonAdame@users.noreply.github.com>
2021-12-12 09:30:46 -0300

    exchanges

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -3,6 +3,7 @@ import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import Loader from './Loader';
 
 const Crytopcurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 50;
@@ -17,7 +18,7 @@ const Crytopcurrencies = ({ simplified }) => {
     setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
 
-  if (isFetching) return "Loading...";
+  if (isFetching) return <Loader/>;
   return (
     <>
       {!simplified && (
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -21,6 +21,9 @@ export const cryptoApi = createApi({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)      
         }),
+        getExchanges: builder.query({
+            query: () => createRequest(`/exchanges`)      
+        }),
         getCryptoDetails: builder.query({
             query: (coinId) => createRequest(`/coin/${coinId}`)
         }),
@@ -31,6 +34,6 @@ export const cryptoApi = createApi({
 });
 
 export const {
-    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery,
+    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery
 } = cryptoApi;
 
